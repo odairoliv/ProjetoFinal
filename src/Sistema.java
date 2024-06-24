afdbcabc93a4a39c1fbc74214854efc5ca4498ea
Odair Oliveira <juniorodair6@gmail.com>
2024-06-24 18:58:34 -0300

    Adicionado função para acessar o sistema (verificando email e senha), Cadastrar usuario e outra para gerar ID para o usuário cadastrado

--- src/Sistema.java
+++ src/Sistema.java
@@ -41,9 +41,56 @@ public class Sistema {
     }
 
     private void entrar(){
+        System.out.println("\n--- Fomulário de Acesso ---");
+        System.out.print("E-mail: ");
+        String email = Console.lerString();
+        System.out.print("Senha: ");
+        String senha = Console.lerString();
+    
+        boolean usuarioEncontrado = false;
+        for (Usuario usuario : usuarios) {
+            if (usuario.getEmail().equals(email)) {
+                usuarioEncontrado = true;
+                if (usuario.getSenha().equals(senha)) {
+                    usuarioAtual = usuario;
+                    menuPrincipal();
+                    return;
+                } else {
+                    System.out.println("\nE-mail ou senha incorretos.");
+                    return;
+                }
+            }
+        }
+        if (!usuarioEncontrado) {
+            System.out.println("\nUsuário não encontrado.");
+        }
     }
 
     private void cadastrar(){
+        System.out.println("\n--- Fomulário de Cadastro ---");
+        System.out.print("Nome Completo: ");
+        String nome = Console.lerString();
+        System.out.print("E-mail: ");
+        String email = Console.lerString();
+        System.out.print("Data de Nascimento: ");
+        String dataNascimento = Console.lerString();
+        System.out.print("CPF: ");
+        String cpf = Console.lerString();
+        System.out.print("Senha: ");
+        String senha = Console.lerString();
+
+        String id = gerarIdUnico();
+        Usuario novoUsuario = new Usuario(id, nome, email, dataNascimento, cpf, senha);
+        usuarios.add(novoUsuario);
+        Arquivo.salvarUsuarios(usuarios);
+        System.out.println("\nUsuário cadastrado com sucesso!");
+    }
+
+    private String gerarIdUnico() {
+        return "U" + (usuarios.size() + 1);
+    }
+
+    private void menuPrincipal(){
 
     }
 }
