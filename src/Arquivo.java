6b7bf1fa4b63b67abd4b273a399ad1f1c38601a1
Odair Oliveira <juniorodair6@gmail.com>
2024-06-24 18:23:06 -0300

    Adicionado opção para pegar dados de ingressos e salvar

--- src/Arquivo.java
+++ src/Arquivo.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 public class Arquivo {
 
     private static final String CAMINHO_USUARIOS = "dados_usuarios.txt";
+    private static final String CAMINHO_INGRESSOS = "dados_ingressos.txt";
 
     public static ArrayList<Usuario> carregarUsuarios() {
         ArrayList<Usuario> usuarios = new ArrayList<>();
@@ -22,6 +23,23 @@ public class Arquivo {
         return usuarios;
     } 
 
+    public static ArrayList<Ingresso> carregarIngressos() {
+        ArrayList<Ingresso> ingressos = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(CAMINHO_INGRESSOS))) {
+            String linha;
+            while ((linha = reader.readLine()) != null) {
+                String[] dados = linha.split(";");
+                if (dados.length == 8) {
+                    Ingresso ingresso = new Ingresso(dados[0], dados[1], dados[2], dados[3], dados[4], dados[5], dados[6], dados[7], Double.parseDouble(dados[8]));
+                    ingressos.add(ingresso);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Erro ao carregar ingressos: " + e.getMessage());
+        }
+        return ingressos;
+    }
+
     public static void salvarUsuarios(ArrayList<Usuario> usuarios) {
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(CAMINHO_USUARIOS))) {
             for (Usuario usuario : usuarios) {
@@ -30,6 +48,17 @@ public class Arquivo {
             }
         } catch (IOException e) {
             System.out.println("Erro ao salvar usuários: " + e.getMessage());
+        }
+    }
+
+    public static void salvarIngressos(ArrayList<Ingresso> ingressos) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(CAMINHO_INGRESSOS))) {
+            for (Ingresso ingresso : ingressos) {
+                bw.write(ingresso.toString());
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Erro ao salvar ingressos: " + e.getMessage());
         }
     }
 }
